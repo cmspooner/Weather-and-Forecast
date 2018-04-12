4fd5dfdb886ed0699510dff81e3608cc36dc0584
Chris Spooner <cmspooner@gmail.com>
2018-04-12 14:35:25 -0400

    Fixes, blocks right size now and sun rise/set

--- settings/index.jsx
+++ settings/index.jsx
@@ -11,6 +11,9 @@ function mySettings(props) {
       <Section
         title={<Text bold align="center">Build Version and Notes</Text>}>
         <Text>
+          Beta 1.2: Areas Right size and Sun rise/set
+        </Text>
+        <Text>
           Beta 1.1: Loading Screen
         </Text>
         <Text>
