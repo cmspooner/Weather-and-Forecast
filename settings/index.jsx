c85f089a1992b7b7843cf16ee8da1a31fce62560
Chris Spooner <cmspooner@gmail.com>
2018-04-16 11:26:19 -0400

    Now with contact info in settings

--- settings/index.jsx
+++ settings/index.jsx
@@ -64,7 +64,37 @@ function mySettings(props) {
         />
       </Section>
       <Section
+        title={<Text bold align="center">Contact Me</Text>}>
+        <Text>
+          Please don't hesitiate to contact me with questions or suggestions. This and all my other apps will always be free and Open Source. If you really like my app please considder buying me a coffee (or more likely electonic components that end up in my classroom). Thanks!
+        </Text>
+        <Link source="mailto:cmspooner@gmail.com">
+          <TextImageRow
+            label="Email"
+            sublabel="cmspooner@gmail.com"
+            icon="https://github.com/cmspooner/Weather-and-Forecast/blob/master/resources/icons/settings/Email.png?raw=true"
+          />
+        </Link>
+        <Link source="https://github.com/cmspooner">
+          <TextImageRow
+            label="Github"
+            sublabel="https://github.com/cmspooner"
+            icon="https://github.com/cmspooner/Weather-and-Forecast/blob/master/resources/icons/settings/Github.png?raw=true"
+          />
+        </Link>
+        <Link source="https://paypal.me/CMSpooner">
+          <TextImageRow
+            label="PayPal"
+            sublabel="cmspooner@gmail.com"
+            icon="https://github.com/cmspooner/Weather-and-Forecast/blob/master/resources/icons/settings/Paypal.png?raw=true"
+          />
+        </Link>
+      </Section>
+      <Section
         title={<Text bold align="center">Build Version and Notes</Text>}>
+        <Text>
+          Beta 1.9: Added links to settings
+        </Text>
         <Text>
           Beta 1.8.1: Copied a little too much from KearsageTime, removed exta unused setting!
         </Text>
